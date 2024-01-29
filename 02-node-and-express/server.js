8fa50ec34004cb2b1a9cefcab511d2a0b3677bb0
p2635 <4522927+p2635@users.noreply.github.com>
2024-01-29 16:45:01 +0000

    Set views property for app

--- 02-node-and-express/server.js
+++ 02-node-and-express/server.js
@@ -1,23 +1,29 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
-app.set('view engine', 'pug');
 
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+// Set up a Template Engine
+app.set("view engine", "pug");
+app.set("views", "./views/pug");
+
+fccTesting(app); // For FCC testing purposes
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
+app.route("/").get((req, res) => {
   // res.render('pug');
-  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
+  res.render("index", {
+    title: "Hello",
+    message: "Please login",
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
